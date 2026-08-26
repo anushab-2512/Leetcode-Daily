cfea1a5d3ed4ee64643f7d816ea4300c7eab0234
anushab-2512 <anushab0012@gmail.com>
2026-08-26 22:15:39 +0530

    Solve To Lower Case problem
    Implement a method to convert a string to lowercase.

---
+++ String/709_To_Lower_Case.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """   
+        res=s.lower()
+        return res
+        
+# Time: O(n) — checks each character.
+# Space: O(n) — a new lowercase string is created.
